ad3c0c874244162355d118f6892742f03c64897e
xczcdjx <xczcdjx@126.com>
2025-11-24 16:24:01 +0800

    转换方法导出，新增受控等值处理

--- testPackage/src/App.vue
+++ testPackage/src/App.vue
@@ -2,18 +2,21 @@
 import {ref} from "vue";
 import {DynamicForm} from "dynamicformdjx";
 import {NaiveUiDynamicForm} from "dynamicformdjx/naiveUi";
-const test=ref<{a:string,b:number,c:number[]}>({
-  a:'1111',
-  b:123,
-  c:[1,3,5]
+
+const test = ref<{ a: string, b: number, c: number[] }>({
+  a: '1111',
+  b: 123,
+  c: [1, 3, 5]
 })
 </script>
 
 <template>
   <div>
     <DynamicForm v-model="test"/>
-    <naive-ui-dynamic-form v-model="test" :dy-list-configs="{arraySplitSymbol:'-'}"/>
-    <p>{{test}}</p>
+    <naive-ui-dynamic-form v-model="test"
+                           :dy-list-configs="{arraySplitSymbol:'-'}"
+                           />
+    <p>{{ test }}</p>
   </div>
 </template>
 
